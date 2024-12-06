c95949ec57beb173becd34995a681902d9708773
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2024-12-06 23:09:19 +0800

    Initial Commit

---
+++ OpenSSL340Test Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenSSL340Test Watch App
+//
+//  Created by Vincent Neo on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenSSL340Test Watch App/OpenSSL340TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSSL340TestApp.swift
+//  OpenSSL340Test Watch App
+//
+//  Created by Vincent Neo on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSSL340Test_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
